859a9e8e33651be883668c722a0a85d603ca802b
italo.crisostomo <italo.crisostomo@DESKTOP-AUXDOC2.etec224.edu.br>
2026-02-27 16:10:42 -0300

    Simples demonstração de funções.

---
+++ src/cursoJavaPoo/Aula68.java
@@ -0,0 +1,52 @@
+package cursoJavaPoo;
+
+import java.util.Scanner;
+
+public class Aula68 {
+	
+	//simples demonstração do funcionamento de funções em java por meio de um programa que lê 3 numeros e devolve o maior entre eles.
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter three numbers: ");
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int higher = max(a, b, c);
+		showResult(higher);
+
+		sc.close();
+
+	}
+
+	public static int max(int x, int y, int z) {
+
+		int aux;
+
+		if (x > y && x > z) {
+
+			aux = x;
+
+		} else if (y > z) {
+
+			aux = y;
+			
+		} else {
+
+			aux = z;
+
+		}
+		return aux;
+
+	}
+	
+	public static void showResult(int value) {
+		
+		System.out.println("\nHigher: " + value);
+	}
+
+}
