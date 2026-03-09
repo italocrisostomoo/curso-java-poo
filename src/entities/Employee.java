ff9b815224bf040d76958277d88662c0cd8a49a1
italo.crisostomo <italo.crisostomo@DESKTOP-AUXDOC2.etec224.edu.br>
2026-03-09 14:24:11 -0300

    Programa para cadastrar funcionário, calcular salário líquido e realizar aumentos.

---
+++ src/entities/Employee.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Employee {
+	
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	public double NetSalary() {
+		
+		return grossSalary - tax;
+	}
+	
+	public void IncreaseSalary(double percentage) {
+		
+		grossSalary += percentage/100 * grossSalary;
+	}
+	
+	public String toString() {
+		
+		return name + ", " + "$" + String.format("%.2f", NetSalary());
+	}
+
+}
